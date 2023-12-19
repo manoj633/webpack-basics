a5ea456583f87a013d021924de630e58a6ac0329
Manoj <manoj09633@gmail.com>
2023-12-19 20:51:40 +0530

    changing event handler to event listener to avoid scope issue

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function changeHeader() {
+document.getElementById("changeHeader").addEventListener("click", () => {
   const el = document.getElementById("header");
   el.innerHTML = "Changed this content through JavaScript;";
-}
+});
